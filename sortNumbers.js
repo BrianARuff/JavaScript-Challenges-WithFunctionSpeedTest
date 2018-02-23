f337d16293e3392180810c1d63c60528a080b76e
Brian Ruff <thelonewolf198927@gmail.com>
2018-02-23 12:42:16 -0500

    sort numbers with modfier

---
+++ sortNumbers.js
@@ -0,0 +1,13 @@
+function getIndexToIns(arr, num) {
+  arr.sort(function(a, b) {
+    return a - b;
+  });
+  for(var i = 0; i < arr.length; i++) {
+    if (num <= arr[i] ) {
+      return i;
+    }
+  }
+  return arr.length;
+}
+
+getIndexToIns([40, 25, 60], 25);
