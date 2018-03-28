563dbd3b724b223a6316fda6f200b00aef965046
Brian Ruff <31252964+BrianARuff@users.noreply.github.com>
2018-03-28 13:14:57 -0400

    Create Correctthemistakesofthecharacterrecognitionsoftware.js

---
+++ Correctthemistakesofthecharacterrecognitionsoftware.js
@@ -0,0 +1,15 @@
+    correct = s => s.replace(/0/g,'O').replace(/1/g,'I').replace(/5/g,'S')
+
+    const correct = string => {
+      return string.split('').map(char => {
+        if (char.match(/5/g)){
+          return char.replace(/5/g, 'S');
+        } else if (char.match(/0/g)){
+          return char.replace(/0/g, 'O');
+        } else if (char.match(/1/g)){
+          return char.replace(/1/g, 'I');
+        } else {
+          return char;
+        }
+      }).join('');
+    };
