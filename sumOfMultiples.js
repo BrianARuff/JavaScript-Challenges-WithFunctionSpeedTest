34c48c1ed2243dc380201b17b2e03ae58042b7f8
Brian Ruff <31252964+BrianARuff@users.noreply.github.com>
2018-03-29 01:18:13 -0400

    Update sumOfMultiples.js

--- sumOfMultiples.js
+++ sumOfMultiples.js
@@ -1,7 +1,7 @@
 //sum of multiples
 
     const sumMul = (n,m) => {
-      if (n >= m) { return 'INVALID' };
+      if (n > m) { return 'INVALID' };
       let sum = 0;
       let ctr = n;
       while (ctr < m) {
