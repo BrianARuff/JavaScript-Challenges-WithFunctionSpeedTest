9bcab823fff633fc0f12445120ea1e248598f562
Brian Ruff <31252964+BrianARuff@users.noreply.github.com>
2018-03-29 01:17:43 -0400

    Create sumOfMultiples.js

---
+++ sumOfMultiples.js
@@ -0,0 +1,12 @@
+//sum of multiples
+
+    const sumMul = (n,m) => {
+      if (n >= m) { return 'INVALID' };
+      let sum = 0;
+      let ctr = n;
+      while (ctr < m) {
+        sum += ctr;
+        ctr += n;
+      }
+      return sum
+    }
